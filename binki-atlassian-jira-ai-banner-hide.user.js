9f870092309c9480911c4ca5f0a06c5b0fa7cac1
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2025-09-25 10:40:59 -0400

    Remove unused dependency.

--- binki-atlassian-jira-ai-banner-hide.user.js
+++ binki-atlassian-jira-ai-banner-hide.user.js
@@ -3,7 +3,6 @@
 // @version 1.0.0
 // @homepageURL https://github.com/binki/binki-atlassian-jira-ai-banner-hide
 // @match https://*.atlassian.net/*
-// @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
 // ==/UserScript==
 
 (async () => {

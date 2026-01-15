0b0aec0fe14b02093338f6c3d8415c8b45faf523
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2026-01-15 15:55:01 -0500

    Hide the Similar requests box in updated Jira.
    Fixes #1.

--- binki-atlassian-jira-ai-banner-hide.user.js
+++ binki-atlassian-jira-ai-banner-hide.user.js
@@ -7,7 +7,9 @@
 
 (async () => {
   const style = document.createElement('style');
-  style.textContent = `div[data-testid="ai-proactive-container.ai-container"] {
+  style.textContent = `div[data-testid="ai-proactive-container.ai-container"],
+  /* see #1. Note that this div is more nested, but I fear that this could easily bleed into hiding the entire issue if the structure of the document changes too much. */
+  div:has(> div > h2[id='smart-related-issues-panel.header.title']) {
     display: none;
   }`;
   document.head.append(style);

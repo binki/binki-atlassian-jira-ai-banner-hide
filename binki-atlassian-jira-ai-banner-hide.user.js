787032cb76e88b2bb319e12c20565fce76ae0f18
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2026-02-23 14:59:59 -0500

    Hide the sidebar suggestions.
    This might hide other “suggestions” but for now we can assume that every suggestion will be about AI.
    
    Fixes #2.

--- binki-atlassian-jira-ai-banner-hide.user.js
+++ binki-atlassian-jira-ai-banner-hide.user.js
@@ -9,7 +9,9 @@
   const style = document.createElement('style');
   style.textContent = `div[data-testid="ai-proactive-container.ai-container"],
   /* see #1. Note that this div is more nested, but I fear that this could easily bleed into hiding the entire issue if the structure of the document changes too much. */
-  div:has(> div > h2[id='smart-related-issues-panel.header.title']) {
+  div:has(> div > h2[id='smart-related-issues-panel.header.title']),
+  /* see #2. Note that this is a more generalized element but in the foreseeable future probably only AI things are going to show up in here. */
+  div[data-testid='side-nav-recommendation.jira-side-nav'] {
     display: none;
   }`;
   document.head.append(style);
